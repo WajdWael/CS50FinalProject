874bcb058dfad2a92b4323deaaeb495d46642f38
unknown <wajdabdalhay@gmail.com>
2021-12-17 17:36:11 +0300

    ADDing updates

--- project5/settings.py
+++ project5/settings.py
@@ -143,9 +143,6 @@ STATICFILES_DIRS = (
 
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'staticfiles/')
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static/'),
-)
 
 django_heroku.settings(locals())
 
